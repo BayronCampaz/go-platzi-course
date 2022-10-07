433ab604b4df53ffb7ca9a6c61ea0f4fffd43bf7
Bayron Campaz <bayron@thinkbridge.com>
2022-10-07 11:59:54 -0500

    Added constant and variable declarations

--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+
+	//Constant declaration
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	//Variable declaration
+	base := 10
+	var heigth int = 15
+	var area int
+
+	fmt.Println(base, heigth, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	const squareBase = 5
+	squareArea := squareBase * squareBase
+
+	fmt.Println(squareArea)
+
 }
