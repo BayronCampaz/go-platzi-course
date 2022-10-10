fb335e8e2009ee1f9c94e82f6230ddf1db6f3db3
Bayron Campaz <bayron@thinkbridge.com>
2022-10-10 10:51:07 -0500

    Maps explanation

--- src/main.go
+++ src/main.go
@@ -1,55 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
+	m := make(map[string]int)
+	m["Jose"] = 14
+	m["Pepito"] = 20
+	fmt.Println(m)
 
-	// Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
-
-	// Slice
-	slice := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Slide methods
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-
-	mySlice := []string{"How", "you", "doing"}
-
-	for i, value := range mySlice {
+	for i, value := range m {
 		fmt.Println(i, value)
 	}
 
-	word := "kayaK"
-	fmt.Println(isPalindrome(word))
-}
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 
-func isPalindrome(word string) bool {
-	result := true
-	word = strings.ToLower(word)
-	for i := 0; i <= len(word)/2; i++ {
-		if word[i] != word[len(word)-i-1] {
-			result = false
-			break
-		}
-	}
+	//What happens when we try to access an unexistent key
+	value2, ok2 := m["Joseph"]
+	fmt.Println(value2, ok2)
 
-	return result
 }
