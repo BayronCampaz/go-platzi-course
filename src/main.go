07c0f70a07d8df3a44bdda917b312e8900145159
Bayron Campaz <bayron@thinkbridge.com>
2022-10-10 13:59:41 -0500

    Struct and pointers explanation

--- src/main.go
+++ src/main.go
@@ -5,8 +5,44 @@ import (
 	pk "go-platzi-course/mypackage"
 )
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "pong")
+}
+
+func (myPC *pc) duplicateRAM() {
+	myPC.ram = myPC.ram * 2
+}
+
 func main() {
 	myCar := pk.Car{Brand: "Tesla", Year: 2020}
 	fmt.Println(myCar)
 	pk.PrintMessage("This works")
+
+	//Pointers
+	a := 50
+	b := &a
+
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPc := pc{ram: 8, disk: 516, brand: "HP"}
+	fmt.Println(myPc)
+	myPc.ping()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
 }
