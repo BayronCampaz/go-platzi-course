5adec454c83f23cd8e41c784f392ec8c7015fef4
Bayron Campaz <bayron@thinkbridge.com>
2022-10-11 10:24:49 -0500

    Completed channels explanation

--- src/main.go
+++ src/main.go
@@ -9,10 +9,38 @@ func say(text string, c chan<- string) {
 	fmt.Println(text)
 }
 
-func main() {
-	c := make(chan string, 1)
-	fmt.Println("Hello")
+func message(text string, c chan string) {
+	c <- text
+}
 
-	go say("Bye", c)
-	fmt.Println(<-c)
+func main() {
+	c := make(chan string, 2)
+	c <- "message1"
+	c <- "message2"
+
+	fmt.Println(len(c), cap(c))
+
+	//Close
+	close(c)
+
+	//Range
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Message1", email1)
+	go message("Message2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email received from email 1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email received from email 2", m2)
+		}
+	}
 }
